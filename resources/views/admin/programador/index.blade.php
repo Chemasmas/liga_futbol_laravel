49ab0324f94bb62e0183fd63f3a0f2fc7d599758
Jorge Sierra <jorge.sierra.florido@gmail.com>
2018-03-03 20:31:41 -0600

    Programadores

--- resources/views/admin/programador/index.blade.php
+++ resources/views/admin/programador/index.blade.php
@@ -1,18 +1,35 @@
 @extends('layout.admin')
 
-@section('titulo','Equipo')
+@section('titulo','Programador')
 
 @section('rol',"")
 
 @section('content')
-
     <div class="row">
         <div class="col-sm-12">
             <h3>Lista de Programadores</h3>
             <div class="col-sm-12">
                 <table class="table table-hover table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Acciones</th>
+                    </tr>
+                    </thead>
+                    @foreach ($programadores as $programador)
+                        <tr>
+                            <td>
+                                {{$programador->nombre}}
+                            </td>
+                            <td>
+                                <a class="btn btn-primary" role="button" href="{{action("ProgramadorController@show",["id"=>$programador->id])}}">Ver</a>
+                                <a class="btn btn-success" role="button" href="{{action("ProgramadorController@edit",["id"=>$programador->id])}}">Editar</a>
+                            </td>
+                        </tr>
+                    @endforeach
                 </table>
             </div>
         </div>
     </div>
 @endsection
+
