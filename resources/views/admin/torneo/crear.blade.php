d04b7b6f45c0eb9640e81ebebae9b67cd82f5f26
Jorge Sierra <jorge.sierra.florido@gmail.com>
2018-03-14 12:59:36 -0600

    Preubas

--- resources/views/admin/torneo/crear.blade.php
+++ resources/views/admin/torneo/crear.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.admin')
 
-@section('titulo',!empty($torneo)?'Editar Torneo':'Crear Torneo')
+@section('titulo',!empty($torneo1)?'Editar Torneo':'Crear Torneo')
 
 @section('rol',"")
 
@@ -10,34 +10,34 @@
             {{csrf_field()}}
             <div class="form-group">
                 <label for="nombre">Nombre del Torneo</label>
-                <input type="text" name="nombre" class="form-control" placeholder=""  value="{{!empty($torneo)?$torneo->nombre:''}}" @if($torneo->generado) disabled @endif>
+                <input type="text" name="nombre" class="form-control" placeholder=""  value="{{!empty($torneo1)?$torneo1->nombre:''}}" @if($torneo1->generado) disabled @endif>
             </div>
             <!--TODO Esto podria tener una tabla-->
             <div class="form-group">
                 <label for="tipo_torneo">Tipo</label>
-                <select class="form-control" name="tipo_torneo" @if($torneo->generado) disabled @endif>
-                    <option value="5" {{!empty($torneo)&&5==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 5</option>
-                    <option value="6" {{!empty($torneo)&&6==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 6</option>
-                    <option value="7" {{!empty($torneo)&&7==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 7</option>
-                    <option value="1" {{!empty($torneo)&&1==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 7 2V</option>
-                    <option value="8" {{!empty($torneo)&&8==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 8</option>
-                    <option value="9" {{!empty($torneo)&&9==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 9</option>
-                    <option value="10" {{!empty($torneo)&&10==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 10</option>
-                    <option value="11" {{!empty($torneo)&&11==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 11</option>
-                    <option value="12" {{!empty($torneo)&&12==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 12</option>
-                    <option value="13" {{!empty($torneo)&&13==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 13</option>
-                    <option value="14" {{!empty($torneo)&&14==$torneo->tipo_torneo?'"selected"':''}}>Plantilla 14</option>
+                <select class="form-control" name="tipo_torneo" @if($torneo1->generado) disabled @endif>
+                    <option value="5" {{!empty($torneo1)&&5==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 5</option>
+                    <option value="6" {{!empty($torneo1)&&6==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 6</option>
+                    <option value="7" {{!empty($torneo1)&&7==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 7</option>
+                    <option value="1" {{!empty($torneo1)&&1==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 7 2V</option>
+                    <option value="8" {{!empty($torneo1)&&8==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 8</option>
+                    <option value="9" {{!empty($torneo1)&&9==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 9</option>
+                    <option value="10" {{!empty($torneo1)&&10==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 10</option>
+                    <option value="11" {{!empty($torneo1)&&11==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 11</option>
+                    <option value="12" {{!empty($torneo1)&&12==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 12</option>
+                    <option value="13" {{!empty($torneo1)&&13==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 13</option>
+                    <option value="14" {{!empty($torneo1)&&14==$torneo1->tipo_torneo?'"selected"':''}}>Plantilla 14</option>
                 </select>
 
             </div>
             <div class="form-group">
                 <label>Género</label>
-                <select class="form-control" name="genero" @if($torneo->generado) disabled @endif>
-                    <option value="M" {{!empty($torneo)&&"M"==$torneo->genero?'"selected"':''}} >Varonil</option>
-                    <option value="F" {{!empty($torneo)&&"F"==$torneo->genero?'"selected"':''}}>Femenil</option>
+                <select class="form-control" name="genero" @if($torneo1->generado) disabled @endif>
+                    <option value="M" {{!empty($torneo1)&&"M"==$torneo1->genero?'"selected"':''}} >Varonil</option>
+                    <option value="F" {{!empty($torneo1)&&"F"==$torneo1->genero?'"selected"':''}}>Femenil</option>
                 </select>
             </div>
-            <button type="submit" class="btn btn-success pull-right" @if($torneo->generado) disabled @endif> {{!empty($torneo)?'Guardar Edición':'Crear'}}</button>
+            <button type="submit" class="btn btn-success pull-right" @if($torneo1->generado) disabled @endif> {{!empty($torneo1)?'Guardar Edición':'Crear'}}</button>
         </form>
     </div>
 
