2c57dc1da2feb756e9c73dc377200e6e46c2a3d7
Unknown <andrea.who.where@gmail.com>
2018-03-17 12:23:11 -0600

    ima3

--- resources/views/admin/arbitro/detail.blade.php
+++ resources/views/admin/arbitro/detail.blade.php
@@ -9,7 +9,7 @@
         <h3>Información de Árbitro</h3>
         <table class="table table-hover table-bordered">
             <tr>
-                <td rowspan="5"><div><img src="{{ asset($arbitro->foto) }}" alt="" class="img-responsive img-thumbnail" style="width: 100%;"></div></td>
+                <td rowspan="5"><div><img src="{{ asset($arbitro->foto) }}" alt="" class="img-responsive img-thumbnail" style="max-height: 20em; max-width: 20em;"></div></td>
                 <td colspan="2"><h4>Nombre : {{$arbitro->nombre}}</h4></td>
             </tr>
             <tr>
@@ -18,7 +18,6 @@
             <tr>
                 <td colspan="3"><h4>Nombre de Usuario : {{$arbitro->usuario->username}}</h4></td>
             </tr>
-
         </table>
     </div>
 @endsection
