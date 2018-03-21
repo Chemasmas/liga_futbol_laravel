45e28834058d3847eb36fc394507d7ea50e038c1
Unknown <andrea.who.where@gmail.com>
2018-03-21 10:53:38 -0600

    Varonil public

--- resources/views/publica/statisticsmale.blade.php
+++ resources/views/publica/statisticsmale.blade.php
@@ -3,7 +3,6 @@
 @section('titulo','Inicio')
 
 @section('content')
-
 	<!-- Page Heading & Breadcrumbs  -->
 	<div class="page-heading-breadcrumbs">
 		<div class="container">
@@ -61,7 +60,7 @@
 
 						<!-- Aside Widget -->
 						<div class="aside-widget">
-							<a href="#"><img src="images/Anuncio.jpeg" alt=""></a>
+							<a href="#"><img src="/images/Anuncio.jpeg" alt=""></a>
 						</div>
 						<!-- Aside Widget -->
 
@@ -70,26 +69,15 @@
 							<h3><span>Categorias</span></h3>
 							<div class="Popular-news">
 								<ul>
-									<li>
-										<img src="images/LOGo final vertical.png" alt="" width="50">
-										<h5><a href="#">Categoria 1</a></h5>
-										<h5><a href="#">ir </a></h5>
-									</li>
-									<li>
-										<img src="images/LOGo final vertical.png" alt="" width="50">
-										<h5><a href="#">categoria 2</a></h5>
-										<h5><a href="#">ir</a></h5>
-									</li>
-									<li>
-										<img src="images/LOGo final vertical.png" alt="" width="50">
-										<h5><a href="#">categoria 3</a></h5>
-										<h5><a href="#">ir </a></h5>
-									</li>
-									<li>
-										<img src="images/LOGo final vertical.png" alt="" width="50">
-										<h5><a href="#">categoria 4</a></h5>
-										<h5><a href="#">ir </a></h5>
-									</li>
+									@foreach($torneosA as $torneo)
+										<li>
+											<img src="/images/LOGo final vertical.png" alt="" width="50">
+											<h5><a href="{{action('HomeController@statisticsmale',["idT"=>$torneo->id])}}">
+													{{$torneo->nombre}}
+												</a></h5>
+											<h5><a href="#">ir </a></h5>
+										</li>
+									@endforeach
 								</ul>
 							</div>
 						</div>
@@ -101,12 +89,12 @@
 							<div class="Popular-news">
 								<ul>
 									<li>
-										<img src="images/LOGo final vertical.png" alt="" width="50">
+										<img src="/images/LOGo final vertical.png" alt="" width="50">
 										<h5><a href="nextmatchs.html">proximos partidos</a></h5>
 										<span class="red-color"><i class="fa fa-clock-o"></i>Fechas</span>
 									</li>
 									<li>
-										<img src="images/LOGo final vertical.png" alt="" width="50">
+										<img src="/images/LOGo final vertical.png" alt="" width="50">
 										<h5><a href="#">resultados de partidos</a></h5>
 										<span class="red-color"><i class="fa fa-clock-o"></i>Fechas</span>
 									</li>
@@ -124,7 +112,7 @@
 
 						<!-- Piont Table -->
 						<div class="macth-fixture">
-						  <h5>Categoria 1</h5>
+						  <h5>{{$torneoCurr->nombre}}</h5>
 						  <div class="last-matches styel-3">
 								<div class="table-responsive">
 									<table class="table table-bordered table-hover">
@@ -139,126 +127,17 @@
 									      	</tr>
 									    </thead>
 									    <tbody>
-									    	<tr>
-										        <td>1</td>
-										        <td><img src="images/small-flags/img-01.png" alt="">Equipo 1</td>
-										        <td>10</td>
-										        <td>12</td>
-										        <td>20</td>
-										        <td>12</td>
-									      	</tr>
-									      	<tr>
-										        <td>2</td>
-										        <td><img src="images/small-flags/img-02.png" alt="">Equipo 2</td>
-										        <td>10</td>
-										        <td>12</td>
-										        <td>20</td>
-										        <td>12</td>
-									      	</tr>
-									      	<tr>
-										        <td>3</td>
-										        <td><img src="images/small-flags/img-03.png" alt="">Equipo 3</td>
-										        <td>20</td>
-										        <td>15</td>
-										        <td>20</td>
-										        <td>15</td>
-								      		</tr>
-								      		<tr>
-										        <td>4</td>
-										        <td><img src="images/small-flags/img-04.png" alt="">Equipo 4</td>
-										        <td>60</td>
-										        <td>10</td>
-										        <td>60</td>
-										        <td>10</td>
-								      		</tr>
-								      		<tr>
-										        <td>5</td>
-										        <td><img src="images/small-flags/img-05.png" alt="">Equipo 5</td>
-										        <td>30</td>
-										        <td>06</td>
-										        <td>30</td>
-										        <td>06</td>
-								      		</tr>
-								      		<tr>
-										        <td>1</td>
-										        <td><img src="images/small-flags/img-01.png" alt="">Equipo 6</td>
-										        <td>10</td>
-										        <td>12</td>
-										        <td>20</td>
-										        <td>12</td>
-									      	</tr>
-									      	<tr>
-										        <td>2</td>
-										        <td><img src="images/small-flags/img-02.png" alt="">Equipo 7</td>
-										        <td>10</td>
-										        <td>12</td>
-										        <td>20</td>
-										        <td>12</td>
-									      	</tr>
-									      	<tr>
-										        <td>3</td>
-										        <td><img src="images/small-flags/img-03.png" alt="">Equipo 8</td>
-										        <td>20</td>
-										        <td>15</td>
-										        <td>20</td>
-										        <td>15</td>
-								      		</tr>
-								      		<tr>
-										        <td>4</td>
-										        <td><img src="images/small-flags/img-04.png" alt="">Equipo 9</td>
-										        <td>60</td>
-										        <td>10</td>
-										        <td>60</td>
-										        <td>10</td>
-								      		</tr>
-								      		<tr>
-										        <td>5</td>
-										        <td><img src="images/small-flags/img-05.png" alt="">Equipo 10</td>
-										        <td>30</td>
-										        <td>06</td>
-										        <td>30</td>
-										        <td>06</td>
-								      		</tr>
-								      		<tr>
-										        <td>1</td>
-										        <td><img src="images/small-flags/img-01.png" alt="">Equipo 10</td>
-										        <td>10</td>
-										        <td>12</td>
-										        <td>20</td>
-										        <td>12</td>
-									      	</tr>
-									      	<tr>
-										        <td>2</td>
-										        <td><img src="images/small-flags/img-02.png" alt="">EQUIPO 11</td>
-										        <td>10</td>
-										        <td>12</td>
-										        <td>20</td>
-										        <td>12</td>
-									      	</tr>
-									      	<tr>
-										        <td>3</td>
-										        <td><img src="images/small-flags/img-03.png" alt="">equipo 12</td>
-										        <td>20</td>
-										        <td>15</td>
-										        <td>20</td>
-										        <td>15</td>
-								      		</tr>
-								      		<tr>
-										        <td>4</td>
-										        <td><img src="images/small-flags/img-04.png" alt="">Equipo 13</td>
-										        <td>60</td>
-										        <td>10</td>
-										        <td>60</td>
-										        <td>10</td>
-								      		</tr>
-								      		<tr>
-										        <td>5</td>
-										        <td><img src="images/small-flags/img-05.png" alt="">Equipo 14</td>
-										        <td>30</td>
-										        <td>06</td>
-										        <td>30</td>
-										        <td>06</td>
-								      		</tr>
+                                        <?php $i=1; ?>
+										@foreach($estadisticas as $estadistica)
+											<tr>
+												<td>{{$i++}}</td>
+												<td><img src="/{{$estadistica->equipo->institucione->escudo}}" alt="" height="50px">{{$estadistica->equipo->nombre}}</td>
+												<td>{{intval($estadistica->Puntos)}}</td>
+												<td>{{intval($estadistica->PartidosGanados)}}</td>
+												<td>{{intval($estadistica->PartidosPerdidos)}}</td>
+												<td>{{intval($estadistica->PartidosEmpatados)}}</td>
+											</tr>
+										@endforeach
 									    </tbody>
 								  	</table>
 							  	</div>
